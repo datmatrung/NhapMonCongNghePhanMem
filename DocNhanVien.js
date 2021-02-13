b87c3477b910be08cbe99a224a70ebf1a6365889
datmatrung <46807674+datmatrung@users.noreply.github.com>
2021-02-13 16:00:42 +0700

    Update DocNhanVien.js

--- DocNhanVien.js
+++ DocNhanVien.js
@@ -1,16 +1,20 @@
-// 1- Họ tên - Mã số
-// 2- Tên đơn vị - Chi nhánh 
-// 3- Giới tính
-// 4- Số lượng ngoại ngữ, tên các ngoại ngữ
+// 1- Họ tên, mã số
+// 2- Tên của đơn vị, tên của chi nhánh 
+// 3- Giới tính (0: Nam, khác 0: Nữ)
+// 4- Số lượng ngoại ngữ, tên của các ngoại ngữ
 // 5- Ngày sinh, tuổi
+
 const { Console } = require("console")
 const FS = require("fs")
 var DuongDan = `C:\\Users\\ADMIN\\Desktop\\NV_01.json`
 var ChuoiJSON = FS.readFileSync(DuongDan)
 var NhanVien = JSON.parse(ChuoiJSON)
 var ChuoiTXT = `${NhanVien.Ho_ten} ${NhanVien.Ma_so}`
-ChuoiTXT += `\n${NhanVien.Gioi_tinh}`
 ChuoiTXT += `\n${NhanVien.Don_vi.Ten} ${NhanVien.Don_vi.Chi_nhanh.Ten}`
+if (Nhan_vien.Gioi_tinh == '0')
+    Chuoi_TXT+=`\nNam`
+else
+    Chuoi_TXT+=`\nNữ`
 var SoNgoaiNgu = NhanVien.Danh_sach_Ngoai_ngu.length
 ChuoiTXT += `\nBiet ${SoNgoaiNgu} ngoai ngu: `
 NhanVien.Danh_sach_Ngoai_ngu.forEach(NgoaiNgu => {
