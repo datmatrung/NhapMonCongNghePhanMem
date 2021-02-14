143f5418023162ab7dcf7929f8bbe02484662646
datmatrung <46807674+datmatrung@users.noreply.github.com>
2021-02-14 12:09:44 +0700

    Update DocNhanVien.js

--- DocNhanVien.js
+++ DocNhanVien.js
@@ -5,15 +5,12 @@
 // 5- Ngày sinh, tuổi
 
 const FS = require("fs")
-var DuongDan = `C:\\Users\\ADMIN\\Desktop\\NV_01.json`
+var DuongDan = `H:\\NMCNPM\\1988106-Nguyen_Hai_Trung-Bai_tap_2\\Du_lieu\\Nhan_vien\\NV_12.json`
 var ChuoiJSON = FS.readFileSync(DuongDan)
 var NhanVien = JSON.parse(ChuoiJSON)
 var ChuoiTXT = `${NhanVien.Ho_ten} ${NhanVien.Ma_so}`
 ChuoiTXT += `\n${NhanVien.Don_vi.Ten} ${NhanVien.Don_vi.Chi_nhanh.Ten}`
-if (NhanVien.Gioi_tinh == '0')
-    ChuoiTXT+=`\nNam`
-else
-    ChuoiTXT+=`\nNữ`
+ChuoiTXT += `\n${NhanVien.Gioi_tinh}`
 var SoNgoaiNgu = NhanVien.Danh_sach_Ngoai_ngu.length
 ChuoiTXT += `\nBiet ${SoNgoaiNgu} ngoai ngu: `
 NhanVien.Danh_sach_Ngoai_ngu.forEach(NgoaiNgu => {
@@ -22,5 +19,5 @@ NhanVien.Danh_sach_Ngoai_ngu.forEach(NgoaiNgu => {
 var NgaySinh = new Date(NhanVien.Ngay_sinh)
 ChuoiTXT += `\n${NgaySinh.getDate()-1}/${NgaySinh.getMonth()+1}/${NgaySinh.getFullYear()}`
 var Tuoi = new Date().getFullYear() - NgaySinh.getFullYear()
-ChuoiTXT += `\n${Tuoi} tuoi`
+ChuoiTXT += ` ${Tuoi} tuoi`
 console.log(ChuoiTXT)
