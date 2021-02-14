af752deb3d5d1041d458b97db39bd437052656bf
datmatrung <46807674+datmatrung@users.noreply.github.com>
2021-02-14 19:38:47 +0700

    Add files via upload

---
+++ DocDanhSachNhanVien.js
@@ -0,0 +1,21 @@
+const FS = require("fs")
+function DocDanhSachNhanVien(){
+    var DuongDanThuMuc = `C:\\Users\\ASUS\\Desktop\\Nhan_vien\\Nhan_vien`
+    var DanhSachTenTapTin = FS.readdirSync(DuongDanThuMuc)
+    var DanhSach = []
+    DanhSachTenTapTin.forEach(ten => {
+        var DuongDan = DuongDanThuMuc + "\\" + ten
+        var ChuoiJSON = FS.readFileSync(DuongDan)
+        var NhanVien = JSON.parse(ChuoiJSON)
+        DanhSach.push(NhanVien)
+    })
+    return DanhSach
+}
+function TaoChuoiTXTDanhSachNhanVien(DanhSach){
+    var ChuoiTXT = `Danh sach ${DanhSach.length} nhan vien\n`
+    ChuoiTXT += DanhSach.map(x => x.Ho_ten).join("\n")
+    return ChuoiTXT
+}
+var DanhSach = DocDanhSachNhanVien()
+var ChuoiTXT = TaoChuoiTXTDanhSachNhanVien(DanhSach)
+console.log(ChuoiTXT)
