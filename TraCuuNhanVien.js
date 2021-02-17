c405d8a5651f4349ca243199c19d70c725d14406
datmatrung <46807674+datmatrung@users.noreply.github.com>
2021-02-17 12:05:54 +0700

    Update TraCuuNhanVien.js

--- TraCuuNhanVien.js
+++ TraCuuNhanVien.js
@@ -1,25 +1,25 @@
 // npm install express
 const Express = require("express")
-const XuLy = require("./XL_3L")
-var UngDung = Express()
-UngDung.use(Express.urlencoded())
-UngDung.listen(3000)
-UngDung.get("/",XL_KhoiDong)
-UngDung.get("/TraCuuNhanVien",XL_TraCuuNhanVien)
+const XuLy = require("./XuLy3Lop")
 
 XL_KhoiDong=(req,res)=>{
-    var ChuoiHTML = `<div>Tra cứu nhân viên (GET) </div>
-                      <form action = '/TraCuuNhanVien' method='get'>
-                      Họ tên <input name ='TH_ChuoiTraCuu'>
-                      <button type ='submit'> Đồng ý </button>
-                      </form>`
+    var ChuoiHTML = `<form action='/TraCuuNhanVien' method='get'>
+                        Họ tên <input name='TH_ChuoiTraCuu' autocomplete = "off">
+                        <button type='submit'> Đồng ý </button>
+                    </form>`
     res.send(ChuoiHTML)
 }
 
 XL_TraCuuNhanVien=(req,res)=>{
-    var ChuoiTraCuu = req.query.TH_ChuoiTraCuu
-    var DanhSach=XuLy.DocDanhSachNhanVien()
+    var DanhSach = XuLy.DocDanhSachNhanVien()
+    var ChuoiTraCuu=req.query.TH_ChuoiTraCuu
     var DanhSachKq=XuLy.TraCuuNhanVienTheoHoTen(DanhSach,ChuoiTraCuu)
     var ChuoiHTML=XuLy.TaoChuoiHTMLDanhSachNhanVien(DanhSachKq)
     res.send(ChuoiHTML)
 }
+
+var UngDung = Express()
+UngDung.use(Express.urlencoded())
+UngDung.listen(3000)
+UngDung.get("/", XL_KhoiDong)
+UngDung.get("/TraCuuNhanVien", XL_TraCuuNhanVien)
